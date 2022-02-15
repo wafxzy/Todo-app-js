2438eb2f85a0dd79516611fe497a76b120e9b7ce
wafxzy <30watt83@gmail.com>
2022-02-15 19:15:00 +0200

    load

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ addtaskbtn.addEventListener("click", function(){
         localStorage.setItem("localtask", JSON.stringify(taskObj));
         addtaskinput.value = '';
     }
-    showtask();
+    uncompletedtaskshow();
 })
 
 // showtask
@@ -31,6 +31,8 @@ function showtask(){
     }
     let html = '';
     let addedtasklist = document.getElementById("addedtasklist");
+    const counter=document.querySelector(".counter");
+    counter.textContent=taskObj.length;
     taskObj.forEach((item, index) => {
 
         if(item.completeStatus==true){
@@ -42,11 +44,201 @@ function showtask(){
         html += `<tr>
                     <th scope="row">${index+1}</th>
                     
-                    <td><button type="button" class="text-success" id=${index}><i class="fa fa-check-square-o"></i>Complete</button></td>
+                    <td><button type="button" class="text-success"  id=${index}><i class="fa fa-check-square-o"></i>Complete</button></td>
                     ${taskCompleteValue}
                     <td><button type="button" onclick="edittask(${index})" class="text-primary"><i class="fa fa-edit"></i>Edit</button></td>
                     <td><button type="button" onclick="deleteitem(${index})" class="text-danger"><i class="fa fa-trash"></i>Delete</button></td>
                 </tr>`;
     });
     addedtasklist.innerHTML = html;
+}
+
+//completed tasks list
+function taskCompleteShow(){
+    taskObj = [];
+    let webtask = localStorage.getItem("localtask");
+    if(webtask == null){
+        taskObj = [];
+    }
+    else{
+        taskObj = JSON.parse(webtask);
+    }    
+    let html = '';
+    let addedtasklist = document.getElementById("addedtasklist");
+    const counter=document.querySelector(".counter");
+    counter.textContent=taskObj.length;
+    var cc=0;
+    taskObj.forEach((item, index) => {
+
+        if(item.completeStatus==true){
+            taskCompleteValue = `<td class="completed">${item.task_name}</td>`;
+       cc++;
+           
+        html += `<tr>
+                    <th scope="row">${index+1}</th>
+                    
+                    <td><button type="button" onclick="uncompletedtaskshow()"  class="text-success" id=${index}><i class="fa fa-check-square-o"></i>Complete</button></td>
+                    ${taskCompleteValue}
+                    <td><button type="button" onclick="edittask(${index})" class="text-primary"><i class="fa fa-edit"></i>Edit</button></td>
+                    <td><button type="button" onclick="deleteitem(${index})" class="text-danger"><i class="fa fa-trash"></i>Delete</button></td>
+                </tr>`;
+    }
+     counter.textContent=cc;
+    }
+            );
+    addedtasklist.innerHTML = html;
+}
+//uncompletedtaskshow
+function uncompletedtaskshow(){
+    taskObj = [];
+    let webtask = localStorage.getItem("localtask");
+    if(webtask == null){
+        taskObj = [];
+    }
+    else{
+        taskObj = JSON.parse(webtask);
+    }    
+    let html = '';
+    let addedtasklist = document.getElementById("addedtasklist");
+    const counter=document.querySelector(".counter");
+    counter.textContent=taskObj.length;
+    var cc=0;
+    taskObj.forEach((item, index) => {
+
+        if(item.completeStatus==false){
+            taskCompleteValue = `<td>${item.task_name}</td>`;
+       cc++;
+           
+        html += `<tr>
+                    <th scope="row">${index+1}</th>
+                    
+                    <td><button type="button"  class="text-success" id=${index}><i class="fa fa-check-square-o" onclick="uncompletedtaskshow()"></i>Complete</button></td>
+                    ${taskCompleteValue}
+                    <td><button type="button" onclick="edittask(${index})" class="text-primary"><i class="fa fa-edit"></i>Edit</button></td>
+                    <td><button type="button" onclick="deleteitem(${index})" class="text-danger"><i class="fa fa-trash"></i>Delete</button></td>
+                </tr>`;
+    } 
+    counter.textContent=cc;
+}
+            );
+    addedtasklist.innerHTML = html;
+}
+
+
+// edittask
+function edittask(index){
+    let saveindex = document.getElementById("saveindex");
+    let addtaskbtn = document.getElementById("addtaskbtn");
+    let savetaskbtn = document.getElementById("savetaskbtn");
+    saveindex.value = index;
+    let webtask = localStorage.getItem("localtask");
+    let taskObj = JSON.parse(webtask); 
+    
+    addtaskinput.value = taskObj[index]['task_name'];
+    addtaskbtn.style.display="none";
+    savetaskbtn.style.display="block";
+}
+
+function saveonblur(){
+    let webtask = localStorage.getItem("localtask");
+    let taskObj = JSON.parse(webtask); 
+    let saveindex = document.getElementById("saveindex").value;
+    
+    for (keys in taskObj[saveindex]) {
+        if(keys == 'task_name'){
+            taskObj[saveindex].task_name = addtaskinput.value;
+        }
+      }
+    savetaskbtn.style.display="none";
+    addtaskbtn.style.display="block";
+    localStorage.setItem("localtask", JSON.stringify(taskObj));
+    addtaskinput.value='';
+    showtask();
+}
+
+// savetask
+let savetaskbtn = document.getElementById("savetaskbtn");
+savetaskbtn.addEventListener("click", function(){
+    let addtaskbtn = document.getElementById("addtaskbtn");
+    let webtask = localStorage.getItem("localtask");
+    let taskObj = JSON.parse(webtask); 
+    let saveindex = document.getElementById("saveindex").value;
+    
+    for (keys in taskObj[saveindex]) {
+        if(keys == 'task_name'){
+            taskObj[saveindex].task_name = addtaskinput.value;
+        }
+      }
+    savetaskbtn.style.display="none";
+    addtaskbtn.style.display="block";
+    localStorage.setItem("localtask", JSON.stringify(taskObj));
+    addtaskinput.value='';
+    showtask();
+})
+
+//complete task
+function completetask(index){
+    let webtask = localStorage.getItem("localtask");
+    let taskObj = JSON.parse(webtask);
+    taskObj[index] = '<span style="text-decoration:line-through">' + taskObj[index] + '</span>';
+    let addedtasklist = document.getElementById("addedtasklist");
+    addedtasklist.addEventListener("click", function(e){
+        console.log(addedtasklist)
+    })
+    localStorage.setItem("localtask", JSON.stringify(taskObj));
+    showtask();
+} 
+
+// complete task
+let addedtasklist = document.getElementById("addedtasklist");
+    addedtasklist.addEventListener("click", function(e){
+        let webtask = localStorage.getItem("localtask");
+        let taskObj = JSON.parse(webtask);
+        
+        let mytarget = e.target;
+        if(mytarget.classList[0] === 'text-success'){
+        let mytargetid = mytarget.getAttribute("id");
+        
+        mytargetpresibling = mytarget.parentElement.previousElementSibling.previousElementSibling;
+            
+            for (keys in taskObj[mytargetid]) {
+                if(keys == 'completeStatus' && taskObj[mytargetid][keys]==true){
+                    taskObj[mytargetid].completeStatus = false;
+                }else if(keys == 'completeStatus' && taskObj[mytargetid][keys]==false){
+                    taskObj[mytargetid].completeStatus = true;
+                }
+              }
+        localStorage.setItem("localtask", JSON.stringify(taskObj));
+        showtask();
+    }
+    })
+
+    // deleteall
+let deleteallbtn = document.getElementById("deleteallbtn");
+deleteallbtn.addEventListener("click", function(){
+    let savetaskbtn = document.getElementById("savetaskbtn");
+    let addtaskbtn = document.getElementById("addtaskbtn");
+    let webtask = localStorage.getItem("localtask");
+    let taskObj = JSON.parse(webtask);
+    if(webtask == null){
+        taskObj = [];
+    }
+    else{
+        taskObj = JSON.parse(webtask);
+        taskObj = [];
+    }
+    savetaskbtn.style.display="none";
+    addtaskbtn.style.display="block";
+    localStorage.setItem("localtask", JSON.stringify(taskObj));
+    showtask();
+
+})
+
+// deleteitem
+function deleteitem(index){
+    let webtask = localStorage.getItem("localtask");
+    let taskObj = JSON.parse(webtask);
+    taskObj.splice(index, 1);
+    localStorage.setItem("localtask", JSON.stringify(taskObj));
+    showtask();
 }
